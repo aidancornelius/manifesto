fa668c78e71df75804e7bb0622a097bd59547f50
Aidan Cornelius-Bell <aidan@cornelius-bell.com>
2026-01-06 22:05:43 +1100

    update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://manifesto.ursa.nz',
   integrations: [svelte()],
   image: {
     service: { entrypoint: 'astro/assets/services/noop' }
